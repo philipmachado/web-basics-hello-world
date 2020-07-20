b82e866801deadeb0579e8dd5c3a12162e3bb862
Philip Machado <philip.machado@gmail.com>
2020-07-20 19:24:55 +1200

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,7 @@ $(function() {
   // console.log will log a message or object to the browser developer console
   console.log("page loaded...");
 
-  $("selector-goes-here").click(
-    var el = document.getElementById("clickMe");
-    if (el.addEventListener)
-        el.addEventListener("click", doFunction, false);
-    else if (el.attachEvent)
-        el.attachEvent('onclick', doFunction);
-  /* function for when the button is clicked goes here */);
+  $("selector-goes-here").click(/* function for when the button is clicked goes here */);
   /*
    * TODO: You will need to use a css selector to get jQuery to find the button element in the page
    * Then you will need to make a new javascript function to do stuff for when the button
